63fdf9931b5621f45cca4b99a2c03c0ec6673e67
Martina <acetomartina141@gmail.com>
2026-06-16 15:48:38 +0200

    classe evento

--- src/main/java/entities/Evento.java
+++ src/main/java/entities/Evento.java
@@ -1,4 +1,106 @@
 package entities;
 
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+// entity fa diventare la classe una tabella
+// da table diamo il nome della tabella
+@Entity
+@Table(name = "eventi")
+// inizia la classe
 public class Evento {
+
+    // chiave primaria
+    @Id
+    // l'id viene generato automaticamente nel DB
+    // non scriveremo evento.setId(1L)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    // tipi di attributi
+    private Long id;
+    private String titolo;
+    private LocalDate dataEvento;
+    private String descrizione;
+    //salviamo gli ENUM come testo nel DB
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+    private int numeroMassimoPartecipanti;
+
+    // costruttore vuoto per Hibernate
+    public Evento(){}
+
+    // costruttore vero
+    public Evento(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti){
+        this.titolo = titolo;
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(TipoEvento tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                ", dataEvento=" + dataEvento +
+                ", descrizione='" + descrizione + '\'' +
+                ", tipoEvento=" + tipoEvento +
+                ", numeroMassimoPartecipanti=" + numeroMassimoPartecipanti +
+                '}';
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/entities/TipoEvento.java
+++ src/main/java/entities/TipoEvento.java
@@ -1,4 +1,6 @@
 package entities;
 
-public class TipoEvento {
+public enum TipoEvento {
+    PUBBLICO,
+    PRIVATO
 }
