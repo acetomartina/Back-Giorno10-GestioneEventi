dd58104131e992e71ee0f786eda07ffa6e834a49
Martina <acetomartina141@gmail.com>
2026-06-17 16:33:16 +0200

    entities - ok

--- src/main/java/entities/Partecipazione.java
+++ src/main/java/entities/Partecipazione.java
@@ -1,4 +1,61 @@
 package entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "partecipazioni")
 public class Partecipazione {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // la stessa persona può avere più partecipazioni
+    @ManyToOne
+    @JoinColumn(name = "persona_id")
+    private Persona persona;
+
+    // molte partecipazioni possono riferirsi allo stesso evento
+    @ManyToOne
+    @JoinColumn(name = "evento_id")
+    private Evento evento;
+
+    @Enumerated(EnumType.STRING)
+    private StatoPartecipazione stato;
+
+    public Partecipazione(){}
+
+    public Partecipazione(Persona persona, Evento evento, StatoPartecipazione stato) {
+        this.persona = persona;
+        this.evento = evento;
+        this.stato = stato;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public StatoPartecipazione getStato() {
+        return stato;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public void setStato(StatoPartecipazione stato) {
+        this.stato = stato;
+    }
+
 }
